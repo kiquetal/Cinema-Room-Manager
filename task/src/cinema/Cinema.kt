71653ce9c43e08d346e2b66f9dab26026333f3cc
Enrique M. Talavera <kiquetal@gmail.com>
2022-04-14 17:04:50 -0400

    Stage 2

--- task/src/cinema/Cinema.kt
+++ task/src/cinema/Cinema.kt
@@ -1,6 +1,37 @@
 package cinema
 
-fun main() {
+const val MINIMUN_SEAT=60
+const val LOW_PRICE = 8
+const val NORMAL_PRICE = 10
+fun main()
+{
+
+    print("Enter the number of rows: ")
+    val rows = readLine()!!.toInt()
+    print("Enter the number of seats in each row: ")
+    val seatsPerRow = readLine()!!.toInt()
+    val price = if (seatsPerRow * rows <= MINIMUN_SEAT)
+        NORMAL_PRICE
+    else
+        LOW_PRICE
+
+    var prices: Int
+    if (price == LOW_PRICE) {
+        val halfRows = rows / 2
+        prices = (halfRows * seatsPerRow) * NORMAL_PRICE
+        prices += (rows - halfRows) * seatsPerRow * LOW_PRICE
+
+    }
+    else
+    {
+        prices = rows * seatsPerRow * NORMAL_PRICE
+    }
+    println("Total income: $$prices")
+
+}
+
+
+fun createScreen() {
     // write your code here
     val rows = 7
     val columns = 8
